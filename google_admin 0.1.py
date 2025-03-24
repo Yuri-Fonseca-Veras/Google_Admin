fa1c42f748883218486b588de47036b4b54c1efe
Yuri-Fonseca-Veras <yurifonsecaveras@outlook.com>
2025-03-24 19:04:20 -0300

    Update google_admin 2.1.py
    Este script exibe até 9 contas para resetar a senha. Em atualizações futuras, planejo expandir essa funcionalidade para exibir mais usuários. Duas opções de senhas são fornecidas no arquivo JSON. Caso a primeira senha não funcione, o script tentará a segunda senha automaticamente.
    
    Além disso, o script desativa o desafio de login por um período de 10 minutos. O desafio de login é uma medida de segurança que exige que o usuário complete uma série de passos adicionais para acessar sua conta, geralmente quando o sistema detecta uma atividade suspeita ou um acesso de um local desconhecido. Ao desativar esse desafio temporariamente, facilita o acesso à conta durante o processo de reset de senha.
    
    Para utilizar essa ferramenta, é necessário ter uma conta Google com acesso administrativo ao Google Admin, pois isso permite resetar senhas de outros usuários na sua organização.

--- google_admin 0.1.py
+++ google_admin 0.1.py
@@ -1,9 +1,7 @@
-# ________________________________Instalações Necessárias________________________________#
+# Instalações Necessárias
 # 1) Playwright: pip install playwright pytest-playwright
 # 2) Playwright navegadores: python -m playwright install
 # 3) TQDM: pip install tqdm
-# 4) Pytz: pip install json
-
 
 from playwright.sync_api import sync_playwright
 import os
@@ -11,9 +9,11 @@ from time import sleep
 from tqdm import tqdm
 import json
 
+# Limpa a tela
 os.system('cls' if os.name == 'nt' else 'clear')
-print("""
 
+# Mensagem de boas-vindas
+print("""
 ───▐▀▄─────────▄▀▌
 ───▐▓░▀▄▀▀▀▀▀▄▀░▓▌
 ───▐░▓░▄▀░░░▀▄░▓░▌
@@ -29,100 +29,161 @@ SENHA DE E-MAIL!
 with open("dados.json", "r") as arquivo:
     dados = json.load(arquivo)
 
-google_email = dados["email"]  
-google_senha = dados["senha"]  
+# Variáveis de login
+google_email = dados["email"]
+google_senha = dados["senha"]
+nova_senha_01 = dados["reset_senha_01"]
+nova_senha_02 = dados["reset_senha_02"]
 last_user_google = None
 ultima_opcao = None
 
 ## Links ##
 google_admin = "https://admin.google.com"
 
-    
+# Inicia o navegador
 with sync_playwright() as p:
-    # Inicia o navegador
-    browser = p.firefox.launch(headless=True)
-    # Abre uma nova página
+    browser = p.firefox.launch(headless=False)
     page = browser.new_page()
 
-# _______ Funções ________#   
+    page.goto(google_admin)
+    # Funções auxiliares
     def texto_console(conteudo):
+        """Limpa a tela e exibe uma mensagem."""
         os.system('cls' if os.name == 'nt' else 'clear')
         print(conteudo)
         sleep(3)
 
-    def up_down(up, down):
+    def up_down(up, down, velocidade=200):
         """Move a seta para cima ou para baixo."""
         for _ in range(up):
             page.keyboard.press("ArrowUp")
+            page.wait_for_timeout(velocidade)
         for _ in range(down):
             page.keyboard.press("ArrowDown")
+            page.wait_for_timeout(velocidade)
 
-    def user(numero_user):
-        # Localizar o elemento pelo XPath e obter o valor do input
+    def user():
+        """Obtém o valor do input do usuário."""
         locator = page.locator('xpath=/html/body/div[8]/div[2]/header/div[2]/div[2]/div[2]/form/div/div/div/div/div/div[1]/input[2]')
-        user_google = locator.input_value()  # Obtém o texto presente no input
-        print(f"{numero_user}: {user_google}")
+        user_google = locator.input_value()
+        return user_google
 
-#________________________________________________________________________________________________#    # Insere a URL da página que você deseja acessar
-    page.goto(google_admin)
-# Defina as etapas do login
+    # Acessa a página do Google Admin
+
+    # Etapas do login
     etapas_login = [
         "Iniciando login...",
         "Preenchendo e-mail...",
         "Clicando em próximo...",
         "Preenchendo senha...",
-        "Clicando em entrar..."
+        "Clicando em entrar...",
     ]
 
-# Crie uma barra de progresso
+    # Barra de progresso para o login
     with tqdm(total=len(etapas_login), desc="Login em Progresso") as barra_progresso:
         # Iniciando login...
         texto_console("Iniciando login...")
         page.wait_for_timeout(5000)
-        barra_progresso.update(1)  # Atualize a barra de progresso
+        barra_progresso.update(1)
 
         # Preenchendo e-mail...
         page.locator('xpath=//*[@id="identifierId"]').fill(google_email)
         page.wait_for_timeout(5000)
-        barra_progresso.update(1)  # Atualize a barra de progresso
+        barra_progresso.update(1)
 
         # Clicando em próximo...
         page.click('xpath=//*[@id="identifierNext"]/div/button/span')
         page.wait_for_timeout(5000)
-        barra_progresso.update(1)  # Atualize a barra de progresso
+        barra_progresso.update(1)
 
         # Preenchendo senha...
         page.locator('xpath=//*[@id="password"]/div[1]/div/div[1]/input').fill(google_senha)
         page.wait_for_timeout(2000)
-        barra_progresso.update(1)  # Atualize a barra de progresso
+        barra_progresso.update(1)
 
         # Clicando em entrar...
         page.click('xpath=//*[@id="passwordNext"]/div/button/span')
         page.wait_for_timeout(5000)
-        barra_progresso.update(1)  # Atualize a barra de progresso
+        barra_progresso.update(1)
 
+    # Limpa a tela e solicita o nome do usuário
     os.system('cls' if os.name == 'nt' else 'clear')
     buscar_user = input("Por favor, informe o nome do usuário ou e-mail que você deseja buscar: ")
     texto_console("Buscando usuário...")
 
-
-    ## Buscando usuário ##
+    # Busca o usuário
     page.wait_for_timeout(5000)
     page.locator('xpath=/html/body/div[8]/div[2]/header/div[2]/div[2]/div[2]/form/div/div/div/div/div/div[1]/input[2]').fill(buscar_user)
-    page.wait_for_timeout(9000)
+    page.wait_for_timeout(8000)
 
-    ## Usuários achados ##
-    texto_console('Usuários encontrador: ')
-    # Inicializar a variável para rastrear o último valor
+    # Exibe usuários encontrados
+    texto_console('Usuários encontrados: ')
 
-    # Verificar interações com base em condições
-    up_down(0, 2)
-    user("1.")
-    up_down(0, 1)
-    user("2.")
-    up_down(0, 1)
-    user("3.")
-    up_down(0, 1)
-    user("4.")
-    up_down(0, 1)
-    user("5.")
+    # Variáveis para rastrear a busca
+    numero_para_busca = 1
+    numero_para_user = 1
+    rodada = 8
+
+    up_down(0, 1, 200)
+    for i in range(9):
+        up_down(0, numero_para_busca)
+
+        if user() == buscar_user:
+            up_down(0, rodada)
+            break
+
+        print(f'{numero_para_user}: {user()}')
+
+        numero_para_user += 1
+        rodada -= 1
+
+    up_down(9, 0, 150)
+
+    # Solicita a escolha do usuário
+    escolha_user = input("Escolha uma opção: ")
+    texto_console("Resetando senha...")
+    escolha_user_feita = int(escolha_user)
+    numero_para_user = 1
+    page.wait_for_timeout(2000)
+
+    # Seleciona o usuário escolhido
+    for i in range(escolha_user_feita):
+        up_down(0, 1)
+    page.keyboard.press('Enter')
+
+    # Tenta resetar a senha
+    try:
+        page.wait_for_timeout(5000)
+        page.locator("xpath=/html/body/div[8]/c-wiz/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div/span/c-wiz/div/div[3]/span/div/div/div[1]/div/span/span/div/div[2]").dblclick()
+        page.wait_for_timeout(5000)
+        page.click('xpath=//*[@id="c79"]')
+    except:
+        print("Senha não pode ser resetada!")
+        browser.close()
+
+    # Função para colocar a nova senha
+    def colocando_senha():
+        page.keyboard.press('Enter')
+        page.wait_for_timeout(3000)
+        page.locator('xpath=//*[@id="yDmH0d"]/div[5]/div/div[2]/span/div/div[2]/div[2]/span').click()
+        page.wait_for_timeout(3000)
+        page.click('xpath=//*[@id="SecurityTab"]/span[2]')
+        page.wait_for_timeout(3000)
+        texto_console("Tirando desafio de login")
+        page.click('xpath=//*[@id="yDmH0d"]/c-wiz/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div/span/div/div/div[3]/span/div/div/div/div[2]/div/div[1]/div[2]/div[7]/div[1]/div/div[1]/header')
+        page.wait_for_timeout(3000)
+        page.click('xpath=//*[@id="yDmH0d"]/c-wiz/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div/span/div/div/div[3]/span/div/div/div/div[2]/div/div[1]/div[2]/div[7]/div[1]/div/div[2]/div/div/div[1]/div/div[2]')
+
+    # Tenta colocar a nova senha
+    try:
+        page.locator('xpath=//*[@id="yDmH0d"]/div[5]/div/div[2]/span/div/span/c-wiz/c-wiz/div[2]/div/div/div[1]/div/div[1]/input').fill(nova_senha_01)
+        print(f"Tentando senha {nova_senha_01}")
+        colocando_senha()
+    except:
+        page.locator('xpath=//*[@id="yDmH0d"]/div[5]/div/div[2]/span/div/span/c-wiz/c-wiz/div[2]/div/div/div[1]/div/div[1]/input').fill(nova_senha_02)
+        print(f"Tentando senha {nova_senha_02}")
+        colocando_senha()
+
+    # Mensagem de sucesso
+    texto_console("Senha resetada!!!")
+    browser.close()
